47cc6cbe92e2b8b235a890e776aaa91c50b4bca5
DPT-HAGITA <146198337+hagita-61@users.noreply.github.com>
2025-07-06 11:55:35 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+if [ ! -f "$MODEL_FILE" ]; then
+  echo "モデルファイルをダウンロードしています..."
+  curl -L "$MODEL_URL" -o "$MODEL_FILE"
+else
+  echo "モデルファイルはすでにあります"
+fi
 
 uvicorn main:app --host 0.0.0.0 --port $PORT
